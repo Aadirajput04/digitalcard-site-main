295ec9c4b6c651327cb8cb1408686ba7d8c33b7d
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-05-25 16:07:32 +0530

    images changing related all java completed

--- form/form.js
+++ form/form.js
@@ -1,105 +1,105 @@
 //first image
 
-document.addEventListener("DOMContentLoaded", function() {
-  var uploadImageInput = document.getElementById("uploadImage");
-  var userImageElement = document.getElementById("userImage");
+document.addEventListener("DOMContentLoaded", function () {
+    var uploadImageInput = document.getElementById("uploadImage");
+    var userImageElement = document.getElementById("userImage");
 
-  userImageElement.addEventListener("click", function() {
-    uploadImageInput.click();
-  });
+    userImageElement.addEventListener("click", function () {
+        uploadImageInput.click();
+    });
 
-  uploadImageInput.addEventListener("change", function(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
+    uploadImageInput.addEventListener("change", function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
 
-    reader.onload = function(e) {
-      userImageElement.src = e.target.result;
-    };
+        reader.onload = function (e) {
+            userImageElement.src = e.target.result;
+        };
 
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  });
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    });
 });
 
 
 
 // secont image
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var pImageInput = document.getElementById("pImage");
     var proImageElement = document.getElementById("Pro-Image");
 
-    proImageElement.addEventListener("click", function() {
-      pImageInput.click();
+    proImageElement.addEventListener("click", function () {
+        pImageInput.click();
     });
 
-    pImageInput.addEventListener("change", function(event) {
-      var file = event.target.files[0];
-      var reader = new FileReader();
+    pImageInput.addEventListener("change", function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
 
-      reader.onload = function(e) {
-        proImageElement.src = e.target.result;
-      };
+        reader.onload = function (e) {
+            proImageElement.src = e.target.result;
+        };
 
-      if (file) {
-        reader.readAsDataURL(file);
-      }
+        if (file) {
+            reader.readAsDataURL(file);
+        }
     });
-  });
+});
 
 // third image
 
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var gImageInput = document.getElementById("gImage");
     var gImageElement = document.getElementById("G-Image");
 
-    gImageElement.addEventListener("click", function() {
-      gImageInput.click();
+    gImageElement.addEventListener("click", function () {
+        gImageInput.click();
     });
 
-    gImageInput.addEventListener("change", function(event) {
-      var file = event.target.files[0];
-      var reader = new FileReader();
+    gImageInput.addEventListener("change", function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
 
-      reader.onload = function(e) {
-        gImageElement.src = e.target.result;
-      };
+        reader.onload = function (e) {
+            gImageElement.src = e.target.result;
+        };
 
-      if (file) {
-        reader.readAsDataURL(file);
-      }
+        if (file) {
+            reader.readAsDataURL(file);
+        }
     });
-  });
+});
 
 
 //fourth image
 
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var qrImageInput = document.getElementById("QrImage");
     var qrImageElement = document.getElementById("Qr-Image");
 
-    qrImageElement.addEventListener("click", function() {
-      qrImageInput.click();
+    qrImageElement.addEventListener("click", function () {
+        qrImageInput.click();
     });
 
-    qrImageInput.addEventListener("change", function(event) {
-      var file = event.target.files[0];
-      var reader = new FileReader();
+    qrImageInput.addEventListener("change", function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
 
-      reader.onload = function(e) {
-        qrImageElement.src = e.target.result;
-      };
+        reader.onload = function (e) {
+            qrImageElement.src = e.target.result;
+        };
 
-      if (file) {
-        reader.readAsDataURL(file);
-      }
+        if (file) {
+            reader.readAsDataURL(file);
+        }
     });
-  });
+});
 
 
 
-  //fifth image
+//fifth image
 
 // Get the input element and the image element using their classes
 const inputFile = document.querySelector('.pro3-img');
@@ -107,7 +107,7 @@ const img = document.querySelector('.Pro3-Image');
 
 // Add an event listener to the image element
 img.addEventListener('click', () => {
-  inputFile.click(); // Simulate a click on the file input element
+    inputFile.click(); // Simulate a click on the file input element
 });
 
 // Add an event listener for the input change event
@@ -115,18 +115,18 @@ inputFile.addEventListener('change', handleImageUpload);
 
 // Function to handle the image upload
 function handleImageUpload(event) {
-  const file = event.target.files[0];
+    const file = event.target.files[0];
 
-  // Create a file reader
-  const reader = new FileReader();
+    // Create a file reader
+    const reader = new FileReader();
 
-  // Set up the file reader onload function
-  reader.onload = function (e) {
-    img.src = e.target.result;
-  };
+    // Set up the file reader onload function
+    reader.onload = function (e) {
+        img.src = e.target.result;
+    };
 
-  // Read the image file as a data URL
-  reader.readAsDataURL(file);
+    // Read the image file as a data URL
+    reader.readAsDataURL(file);
 }
 
 
@@ -134,10 +134,28 @@ function handleImageUpload(event) {
 
 
 
-  //sixth image
+//sixth image
 
 
+const images = document.querySelectorAll('.rew-Img');
+const inputs = document.querySelectorAll('.R-Img');
 
+images.forEach((image, index) => {
+    image.addEventListener('click', () => {
+        inputs[index].click();
+    });
+
+    inputs[index].addEventListener('change', () => {
+        const file = inputs[index].files[0];
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            image.src = reader.result;
+        });
+
+        reader.readAsDataURL(file);
+    });
+});
 
 
 
@@ -145,14 +163,14 @@ function handleImageUpload(event) {
 
 // rating
 
-  $(document).ready(function() {
-    $('form').submit(function(event) {
-      event.preventDefault();
-      var rating = $('input[name="rating"]:checked').val();
+$(document).ready(function () {
+    $('form').submit(function (event) {
+        event.preventDefault();
+        var rating = $('input[name="rating"]:checked').val();
 
-      console.log("Rating: " + rating);
+        console.log("Rating: " + rating);
     });
-  });
+});
 
 
 
@@ -166,26 +184,26 @@ function handleImageUpload(event) {
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     var currentStep = 1;
     var totalSteps = $('.container').length;
 
     $('.container').hide();
     $('.container:first').show();
 
-    $('.btn-next').click(function() {
-      if (currentStep < totalSteps) {
-        $('.container').hide();
-        $('.container:eq(' + currentStep + ')').show();
-        currentStep++;
-      }
+    $('.btn-next').click(function () {
+        if (currentStep < totalSteps) {
+            $('.container').hide();
+            $('.container:eq(' + currentStep + ')').show();
+            currentStep++;
+        }
     });
 
-    $('.btn-prev').click(function() {
-      if (currentStep > 1) {
-        $('.container').hide();
-        $('.container:eq(' + (currentStep - 2) + ')').show();
-        currentStep--;
-      }
+    $('.btn-prev').click(function () {
+        if (currentStep > 1) {
+            $('.container').hide();
+            $('.container:eq(' + (currentStep - 2) + ')').show();
+            currentStep--;
+        }
     });
-  });
+});
