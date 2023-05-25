3ab4a97d1e63ba1a77ea86940a6c4ba233d0f4e1
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-05-25 12:33:48 +0530

    java script for image change

--- form/form.js
+++ form/form.js
@@ -1,54 +1,143 @@
 // company logo image change
 
 
-function handleImageUpload(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
+document.addEventListener("DOMContentLoaded", function() {
+    var uploadImageInput = document.getElementById("uploadImage");
+    uploadImageInput.addEventListener("change", handleImageUpload);
 
-    reader.onload = function(e) {
-        var imageElement = document.getElementById("userImage");
-        imageElement.src = e.target.result;
+    function handleImageUpload(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var imageElement = document.getElementById("userImage");
+            if (file) {
+                imageElement.src = e.target.result;
+            } else {
+                imageElement.src = "/card tamplate/img/7309681.jpg";
+            }
+        }
+
+        reader.readAsDataURL(file);
     }
-
-    reader.readAsDataURL(file);
-}
+});
 
 
-function handleImageUpload(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
 
-    reader.onload = function(e) {
-        var imageElement = document.getElementById("pImage");
-        imageElement.src = e.target.result;
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var uploadImageInput = document.getElementById("uploadImage");
+    uploadImageInput.addEventListener("change", handleImageUpload);
+
+    function handleImageUpload(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var imageElement = document.getElementById("Pro-Image");
+            if (file) {
+                imageElement.src = e.target.result;
+            } else {
+                imageElement.src = "/card tamplate/img/7309681.jpg";
+            }
+        }
+
+        reader.readAsDataURL(file);
     }
+});
 
-    reader.readAsDataURL(file);
-}
 
-function handleImageUpload(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
 
-    reader.onload = function(e) {
-        var imageElement = document.getElementById("gImage");
-        imageElement.src = e.target.result;
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var uploadImageInput = document.getElementById("uploadImage");
+    uploadImageInput.addEventListener("change", handleImageUpload);
+
+    function handleImageUpload(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var imageElement = document.getElementById("G-Image");
+            if (file) {
+                imageElement.src = e.target.result;
+            } else {
+                imageElement.src = "/card tamplate/img/7309681.jpg";
+            }
+        }
+
+        reader.readAsDataURL(file);
     }
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var uploadImageInput = document.getElementById("uploadImage");
+    uploadImageInput.addEventListener("change", handleImageUpload);
 
-    reader.readAsDataURL(file);
-}
-function handleImageUpload(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
+    function handleImageUpload(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
 
-    reader.onload = function(e) {
-        var imageElement = document.getElementById("PyImage");
-        imageElement.src = e.target.result;
+        reader.onload = function(e) {
+            var imageElement = document.getElementById("Qr-Image");
+            if (file) {
+                imageElement.src = e.target.result;
+            } else {
+                imageElement.src = "/card tamplate/img/7309681.jpg";
+            }
+        }
+
+        reader.readAsDataURL(file);
     }
+});
 
-    reader.readAsDataURL(file);
-}
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var uploadImageInput = document.getElementById("uploadImage");
+    uploadImageInput.addEventListener("change", handleImageUpload);
+
+    function handleImageUpload(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var imageElement = document.getElementsByClassName("Pro2-Image")[0];
+            if (e.target.result) {
+                imageElement.src = e.target.result;
+            } else {
+                imageElement.src = "/card tamplate/img/7309681.jpg";
+            }
+        }
+
+        reader.readAsDataURL(file);
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var uploadImageInput = document.getElementById("uploadImage");
+    uploadImageInput.addEventListener("change", handleImageUpload);
+
+    function handleImageUpload(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var imageElement = document.getElementsByClassName("rew-Img")[0];
+            if (e.target.result) {
+                imageElement.src = e.target.result;
+            } else {
+                imageElement.src = "/card tamplate/img/7309681.jpg";
+            }
+        }
+
+        reader.readAsDataURL(file);
+    }
+});
 
 
 
@@ -59,7 +148,7 @@ function handleImageUpload(event) {
     $('form').submit(function(event) {
       event.preventDefault();
       var rating = $('input[name="rating"]:checked').val();
-      // Process the rating value (e.g., send it to the server via AJAX)
+
       console.log("Rating: " + rating);
     });
   });
@@ -69,7 +158,7 @@ function handleImageUpload(event) {
 
 
 
-  // next and prev button
+
 
 
 
@@ -80,21 +169,21 @@ $(document).ready(function() {
     var currentStep = 1;
     var totalSteps = $('.container').length;
 
-    $('.container').hide(); // Hide all containers initially
-    $('.container:first').show(); // Show the first container
+    $('.container').hide();
+    $('.container:first').show();
 
     $('.btn-next').click(function() {
       if (currentStep < totalSteps) {
-        $('.container').hide(); // Hide all containers
-        $('.container:eq(' + currentStep + ')').show(); // Show the next container
+        $('.container').hide();
+        $('.container:eq(' + currentStep + ')').show();
         currentStep++;
       }
     });
 
     $('.btn-prev').click(function() {
       if (currentStep > 1) {
-        $('.container').hide(); // Hide all containers
-        $('.container:eq(' + (currentStep - 2) + ')').show(); // Show the previous container
+        $('.container').hide();
+        $('.container:eq(' + (currentStep - 2) + ')').show();
         currentStep--;
       }
     });
